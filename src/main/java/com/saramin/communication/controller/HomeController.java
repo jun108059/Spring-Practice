d2889de8e4be9a2bccadcd3088483a92b6e845fb
YoungJun Park <youngjun108059@gmail.com>
2021-04-22 23:22:23 +0900

    [Demo] 테스트 컨트롤러 & Junit 작성

---
+++ src/main/java/com/saramin/communication/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.saramin.communication.controller;
+
+import com.saramin.communication.service.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+
+}
