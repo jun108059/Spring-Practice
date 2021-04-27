76da4d8dc130c750f67dbbcbb6c0ddbe1bd7658f
YoungJun Park <youngjun108059@gmail.com>
2021-04-27 23:28:41 +0900

    [도메인] 주문 도메인 추가

---
+++ src/main/java/com/saramin/communication/order/Order.java
@@ -0,0 +1,28 @@
+package com.saramin.communication.order;
+
+import lombok.Data;
+
+@Data
+public class Order {
+    private Long memberId;
+    private String itemName;
+    private int itemPrice;
+    private int discount;
+
+    public Order(Long memberId, String itemName, int itemPrice, int discountPrice) {
+        this.memberId = memberId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.discount = discountPrice;
+    }
+
+    public Order(Long memberId, String itemName, int itemPrice) {
+        this.memberId = memberId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+    }
+
+    public int calculatePrice() {
+        return itemPrice - discount;
+    }
+}
