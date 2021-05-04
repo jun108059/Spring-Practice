126f2e7c7b67bf2b9c81eb274df1f58688d556d8
YoungJun Park <youngjun108059@gmail.com>
2021-05-05 02:26:13 +0900

    [서비스] 주문 DI 적용

--- src/main/java/com/saramin/communication/order/OrderServiceImpl.java
+++ src/main/java/com/saramin/communication/order/OrderServiceImpl.java
@@ -1,16 +1,18 @@
 package com.saramin.communication.order;
 
 import com.saramin.communication.discount.DiscountPolicy;
-import com.saramin.communication.discount.RateDiscountPolicy;
 import com.saramin.communication.member.Member;
 import com.saramin.communication.member.MemberRepository;
-import com.saramin.communication.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
