165a602bef791c95bd6374c0db234871563c0565
YoungJun Park <youngjun108059@gmail.com>
2021-05-06 23:38:41 +0900

    [테스트] 회원 서비스 기능 테스트

--- src/test/java/com/saramin/communication/member/MemberServiceTest.java
+++ src/test/java/com/saramin/communication/member/MemberServiceTest.java
@@ -1,16 +1,27 @@
 package com.saramin.communication.member;
 
+import com.saramin.communication.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class MemberServiceTest {
+class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    /**
+     * 각 테스트 실행시 한번 씩 호출
+     */
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
         // given
-        Member member = new Member(1L, "memberA", Grade.VIP);
+        Member member = new Member(1L, "Hi", Grade.BASIC);
 
         // when
         memberService.join(member);
@@ -18,5 +29,7 @@ public class MemberServiceTest {
 
         // then
         Assertions.assertThat(member).isEqualTo(findMember);
+
     }
+
 }
