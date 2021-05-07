8846d7f3f806515054d02a23a08677bdc7f87b40
YoungJun Park <youngjun108059@gmail.com>
2021-05-07 23:10:52 +0900

    [테스트] 주문 서비스 기능 테스트

--- src/test/java/com/saramin/communication/order/OrderServiceTest.java
+++ src/test/java/com/saramin/communication/order/OrderServiceTest.java
@@ -1,16 +1,29 @@
 package com.saramin.communication.order;
 
+import com.saramin.communication.AppConfig;
 import com.saramin.communication.member.Grade;
 import com.saramin.communication.member.Member;
 import com.saramin.communication.member.MemberService;
-import com.saramin.communication.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    AppConfig appConfig = new AppConfig();
+    MemberService memberService;
+    OrderService orderService;
+
+    /**
+     * 각 테스트 실행시 한번 씩 호출
+     */
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
+
 
     @Test
     void createOrder() {
