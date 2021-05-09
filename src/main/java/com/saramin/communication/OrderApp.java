d0d071594e9282761bbb3c1ef92a97a79774e3bc
YoungJun Park <youngjun108059@gmail.com>
2021-05-10 00:14:36 +0900

    [Refact] 주문 App 스프링 컨테이너 적용(DI)

--- src/main/java/com/saramin/communication/OrderApp.java
+++ src/main/java/com/saramin/communication/OrderApp.java
@@ -5,13 +5,20 @@ import com.saramin.communication.member.Member;
 import com.saramin.communication.member.MemberService;
 import com.saramin.communication.order.Order;
 import com.saramin.communication.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
-    public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
 
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+    public static void main(String[] args) {
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "itemA", Grade.VIP);
