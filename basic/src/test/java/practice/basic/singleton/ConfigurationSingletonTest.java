50b1fb63cee36ba72fe19af960f8fa1ca272ae63
YoungJun Park <youngjun108059@gmail.com>
2022-07-12 23:46:28 +0900

    ✅ 스프링 컨테이너 싱글톤 테스트

---
+++ basic/src/test/java/practice/basic/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,43 @@
+package practice.basic.singleton;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import practice.basic.AppConfig;
+import practice.basic.member.MemberRepository;
+import practice.basic.member.MemberServiceImpl;
+import practice.basic.order.OrderServiceImpl;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        // 값 3개가 같다
+        System.out.println("memberService -> memberRepository1 = " + memberRepository1);
+        System.out.println("orderService -> memberRepository2 = " + memberRepository2);
+        System.out.println("memberRepository -> memberRepository3 = " + memberRepository);
+
+        Assertions.assertSame(memberRepository1, memberRepository2);
+        Assertions.assertSame(memberRepository1, memberRepository);
+
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+        // 결과: bean = class practice.basic.AppConfig$$EnhancerBySpringCGLIB$$84e8d0a8
+    }
+}
