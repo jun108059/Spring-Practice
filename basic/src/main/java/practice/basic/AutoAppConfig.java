886a093d719c44cb62c5ca9b1cb25366fa71925e
YoungJun Park <youngjun108059@gmail.com>
2023-05-22 23:54:17 +0900

    👔 컴포넌트 스캔 - 중복 등록, 충돌 테스트 (#1)
    스프링 핵심 원리 - 기본편 (#1)

--- basic/src/main/java/practice/basic/AutoAppConfig.java
+++ basic/src/main/java/practice/basic/AutoAppConfig.java
@@ -1,8 +1,11 @@
 package practice.basic;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import practice.basic.member.MemberRepository;
+import practice.basic.member.MemoryMemberRepository;
 
 @Configuration
 @ComponentScan(
@@ -11,5 +14,8 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
